c4efd7c9cd3ce1d61c7edec6051f673414b491b2
blzK <bpertev@gmail.com>
2014-04-18 15:17:55 +0200

    ok...1

---
+++ src/shape.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <g3x.h>
+
+
+  typedef struct{
+  	double c[16];
+  } HMat;
+
+  typedef struct Shape
+  {
+	G3Xpoint *vrtx;/*pointeurs vers les tableaux du vertex canonique*/
+	G3Xvector *norm;/*pointeurs vers les tableaux du normales canonique*/
+	-t Md, Mi, Mn; /*Matrices 4x4 directe, inverse, normale*/
+
+	G3Xcolor col; /*Couleur*/
+  	int ID; /*type*/
+	double ambi, diff, spec, shine, alpha; /*materiau*/
+  } Shape;
